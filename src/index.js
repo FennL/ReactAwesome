31ffeef56c3fc7521ebf79e87000e528b9f2e3ac
Liuq Feng <liufeng0194@outlook.com>
2022-07-03 18:17:37 +0800

    Initial commit and make tick component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"; // JSX编译时候使用到的
+import ReactDOM from "react-dom/client"; // ReactDOM是创建根元素的时候使用到的
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const react = "React!!";
+
+// function tick() {
+//   const element = (
+//     <div>
+//       <h1>Hello World</h1>
+//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//     </div>
+//   );
+//   // 先在HTML中找到root标签，然后创建React-Root标签再渲染
+//   ReactDOM.createRoot(document.getElementById("root")).render(element);
+// }
+
+// setInterval(tick, 1000);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <h1>Hello React</h1>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
