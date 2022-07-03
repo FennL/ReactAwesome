7bd49b6901662ebbdc6e8bc0f6d7672fb241ab62
Liuq Feng <liufeng0194@outlook.com>
2022-07-03 18:44:49 +0800

    first commit HelloWorld

--- README.md
+++ README.md
@@ -14,3 +14,7 @@ npx create-react-app react1
 ## JSX 语法
 
 JSX 语法实际上就是`js`和`HTML`语法的混合，使用`js`的时候需要使用{}写上简单的表达式
+
+## 组件化
+
+组件化其实就是模块化
--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
 import React from "react"; // JSX编译时候使用到的
 import ReactDOM from "react-dom/client"; // ReactDOM是创建根元素的时候使用到的
-
+import HelloWorld from "./components/helloworld";
 const react = "React!!";
 
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello World</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  // 先在HTML中找到root标签，然后创建React-Root标签再渲染
-  ReactDOM.createRoot(document.getElementById("root")).render(element);
-}
+// function tick() {
+//   const element = (
+//     <div>
+//       <h1>Hello World</h1>
+//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//     </div>
+//   );
+//   // 先在HTML中找到root标签，然后创建React-Root标签再渲染
+//   ReactDOM.createRoot(document.getElementById("root")).render(element);
+// }
 
-setInterval(tick, 1000);
+// setInterval(tick, 1000);
 
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <h1>Hello React</h1>
-// );
+ReactDOM.createRoot(document.getElementById("root")).render(<HelloWorld />);
